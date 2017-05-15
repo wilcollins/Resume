2853c923216a531f83be2c7289b2452881014493
Wil Collins <wil.collins@gmail.com>
2017-05-15 11:07:43 -0700

    rebase

---
+++ README.md
@@ -0,0 +1,1 @@
+![alt tag](https://raw.githubusercontent.com/wilcollins/Resume/master/out/resume.png)
---
+++ rebase.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git checkout --orphan latest_branch
+git add -A
+git commit -am "rebase"
+git branch -D master
+git branch -m master
+git push -f origin master
+
